060103261a137d1d6db9e98095c2c3d31cd109e4
shiharu9 <shiharu.nika@gmail.com>
2021-08-25 21:17:09 +0300

    Create templates in app "home"
    * Create controllers in 'home/views.py'
    * Create urls.py in app 'home'
    * Add relative urls paths in 'home/urls.py'
    * Add route between 'life/urls.py' and 'home/urls.py'

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from home.views import *
+
+urlpatterns = [
+    path('', home_page),
+    path('childhood/', childhood),
+    path('youth/', youth),
+    path('oldness/', oldness),
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from .models import Event
 # Create your views here.
+
+
+def home_page(request):
+    data = "Hello"
+    return render(request, 'home/home_page.html', {'data': data})
+
+
+def childhood(request):
+    data = Event.objects.filter(period__name='Детство')
+    return render(request, 'home/childhood.html', {'data': data})
+
+
+def youth(request):
+    data = Event.objects.filter(period__name='Молодость')
+    return render(request, 'home/youth.html', {'data': data})
+
+
+def oldness(request):
+    data = Event.objects.filter(period__name='Старость')
+    return render(request, 'home/oldness.html', {'data': data})
