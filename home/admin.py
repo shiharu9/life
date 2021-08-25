88f488ee8e8a03d220110b67fdc8d1f0abe661b2
shiharu9 <shiharu.nika@gmail.com>
2021-08-25 17:59:21 +0300

    Create two models "Event" and "Period"
    * Add code in admin.py for add models in the admin area
    * Add in admin.py display "Event" model fields in the admin area

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Event, Period
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'period', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content', 'period')
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Period)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=20, verbose_name='Event name')
+    content = models.TextField(verbose_name='Description')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Published')
+    period = models.ForeignKey('Period', on_delete=models.PROTECT)
+
+    class Meta:
+        ordering = ['published']
+
+
+class Period(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
